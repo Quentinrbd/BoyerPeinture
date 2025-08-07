5597c65c27e3b17e4b7a79abb2a0dc6e73e6aac7
Quentinrbd <quentin.rbd@gmail.com>
2025-08-07 14:55:33 +0200

    update photo

--- src/data/reaData.js
+++ src/data/reaData.js
@@ -15,7 +15,7 @@ const ReaData = [
         id: 3,
         imgSrc: '/realisations/porte.jpg',
         alt: 'porte',
-        categorie: 'boiserie'
+        categorie: 'boiserie',
       },
       {
         id: 4,
@@ -33,7 +33,7 @@ const ReaData = [
         id: 6,
         imgSrc: '/realisations/mur.jpg',
         alt: 'mur',
-        categorie: 'interieur'
+        categorie: 'intérieur'
       },
       {
         id: 7,
@@ -45,7 +45,7 @@ const ReaData = [
         id: 8,
         imgSrc: '/realisations/salon.jpg',
         alt: 'salon',
-        categorie: 'interieur'
+        categorie: 'intérieur'
       },
       {
         id: 9,
@@ -81,19 +81,47 @@ const ReaData = [
         id: 14,
         imgSrc: '/realisations/mur2.jpg',
         alt: 'Mur',
-        categorie: 'interieur'
+        categorie: 'intérieur'
       },
       {
         id: 15,
         imgSrc: '/realisations/facade.jpg',
         alt: 'Façade',
-        categorie: 'exterieur'
+        categorie: 'extérieur'
       },
       {
         id: 16,
         imgSrc: '/realisations/facade2.jpg',
         alt: 'Façade',
-        categorie: 'exterieur'
+        categorie: 'extérieur'
+      },
+      {
+        id: 16,
+        imgSrc: '/realisations/avantapres1.jpg',
+        alt: 'Façade',
+        categorie: 'avant/après',
+        desc: "Décapage par ponçage d'un parquet massif, puis finition avec vitrificateur brillant"
+      },
+      {
+        id: 16,
+        imgSrc: '/realisations/avantapres2.jpg',
+        alt: 'Façade',
+        categorie: 'avant/après',
+        desc: "Création de sous bassement avec des moulures"
+      },
+      {
+        id: 16,
+        imgSrc: '/realisations/avantapres3.jpg',
+        alt: 'Façade',
+        categorie: 'avant/après',
+        desc: "Impression de profondeur pour une tête de lit"
+      },
+      {
+        id: 16,
+        imgSrc: '/realisations/avantapres4.jpg',
+        alt: 'Façade',
+        categorie: 'avant/après',
+        desc: "Mise en peinture d'un portail extérieur"
       },
 ]
 export default ReaData;
